1781c65ff9316f7f16c7b78e3e6c8c24a40cdf8e
William Chenyin <welcomehomemutms7@gmail.com>
2025-12-01 01:35:15 -0500

    first commit

---
+++ q1a.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class q1a {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int curr = 50;
+        while (sc.hasNextLine()) {
+            String in = sc.nextLine()
+            if (in.charAt(0) == 'L') {
+                
+            } else {
+
+            }
+        }
+    }
+}
