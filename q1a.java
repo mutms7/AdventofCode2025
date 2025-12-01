c3dbbb09bfdc6db5cd392a4040c3c4dd9a2bdddb
William Chenyin <welcomehomemutms7@gmail.com>
2025-12-01 17:43:51 -0500

    m

--- q1a.java
+++ q1a.java
@@ -4,13 +4,19 @@ public class q1a {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int curr = 50;
+        int password = 0;
         while (sc.hasNextLine()) {
-            String in = sc.nextLine()
+            String in = sc.nextLine();
             if (in.charAt(0) == 'L') {
-                
+                curr -= Integer.parseInt(in.substring(1));
             } else {
-
+                curr += Integer.parseInt(in.substring(1));
+            }
+            curr %= 100;
+            if (curr == 0) {
+                password++;
             }
         }
+        System.out.println(password);
     }
 }
